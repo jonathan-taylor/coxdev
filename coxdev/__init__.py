3fbf44fe0dd98f275a50b1bd33a72469b2a76fa7
Jonathan Taylor <jonathan.taylor@stanford.edu>
2026-02-23 10:50:10 -0800

    fixing __version__

--- coxdev/__init__.py
+++ coxdev/__init__.py
@@ -17,5 +17,18 @@ coxdev.base : Core Cox model implementation.
 coxdev.stratified : Stratified Cox model implementation.
 """
 
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("coxdev")
+except PackageNotFoundError:
+    # package is not installed, perhaps we are in a git repo
+    try:
+        from setuptools_scm import get_version
+        __version__ = get_version(root='..', relative_to=__file__)
+    except (ImportError, LookupError):
+        __version__ = "unknown"
+
 from .base import CoxDeviance
 from .stratified import StratifiedCoxDeviance
+
